8aec4236d2b6416ed7a7559757e5e3cfe57bd436
hillium <maruruku@stu.csust.edu.cn>
2018-07-07 14:55:35 +0800

    让 Main 函数变得更加友好了一些。

--- Main.hs
+++ Main.hs
@@ -1,7 +1,12 @@
 module Main where
 import StringParser
+import System.Environment (getArgs)
 import Text.Parsec (parse)
 import Control.Monad.Random
+
+noInputMessage :: String
+noInputMessage = "No args detached...\n" ++ 
+                 "Entering interactive mode...\n"
 
 mkRandomString :: String -> IO [String]
 mkRandomString s = case parse simpleParser' "" s of
@@ -13,9 +18,24 @@ mkRandomString s = case parse simpleParser' "" s of
         let res' = concat res
         return res'
 
+mainInteract :: IO ()
+mainInteract = do
+    putStr "> "
+    line <- getLine
+    cho <- mkRandomStringFrom line
+    putStrLn cho
+
+mkRandomStringFrom :: String -> IO String
+mkRandomStringFrom s = do
+    dealed <- mkRandomString s
+    cho <- sequence $ uniform <$> dealed
+    return cho
+
 main :: IO ()
 main = do
-    line <- getLine
-    dealed <- mkRandomString line
-    cho <- sequence $ uniform <$> dealed
-    putStrLn cho
+    args <- getArgs
+    case args of
+        [] -> putStr noInputMessage >> forever mainInteract
+        _ ->  forM args (\l -> mkRandomStringFrom l >>= putStrLn) >> return ()
+
+    
