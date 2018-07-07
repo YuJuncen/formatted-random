208ffcb1face70be3d41a867fd6f4d89fe545005
hillium <maruruku@stu.csust.edu.cn>
2018-07-07 17:28:52 +0800

    子串的生成还有重复。

--- Main.hs
+++ Main.hs
@@ -7,35 +7,25 @@ import Control.Monad.Random
 noInputMessage :: String
 noInputMessage = "No args detached...\n" ++ 
                  "Entering interactive mode...\n"
+    
 
-mkRandomString :: String -> IO [String]
-mkRandomString s = case parse simpleParser' "" s of
-    Left err -> return [show err]
-    Right s -> return s
-    where
-    simpleParser' = do
-        res <- simpleParser
-        let res' = concat res
-        return res'
+mkRandomSyntax :: String -> IO String
+mkRandomSyntax s = case parse simpleParser  "" s of
+    Left err -> return $ show err
+    Right res -> (fmap concat. sequence . fmap charify) res 
 
 mainInteract :: IO ()
 mainInteract = do
     putStr "> "
     line <- getLine
-    cho <- mkRandomStringFrom line
+    cho <- mkRandomSyntax line
     putStrLn cho
-
-mkRandomStringFrom :: String -> IO String
-mkRandomStringFrom s = do
-    dealed <- mkRandomString s
-    cho <- sequence $ uniform <$> dealed
-    return cho
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
         [] -> putStr noInputMessage >> forever mainInteract
-        _ ->  forM args (\l -> mkRandomStringFrom l >>= putStrLn) >> return ()
+        _ ->  forM args (\l -> mkRandomSyntax l >>= putStrLn) >> return ()
 
     
--- StringParser.hs
+++ StringParser.hs
@@ -4,71 +4,113 @@
 module StringParser where
 
 import Text.Parsec
+import Control.Monad.Random
 import Text.Parsec.Char
 import Data.List(nub, (\\))
 import qualified Text.Parsec.Token as P
 import Text.Parsec.Language (haskellDef)
 
-data EscapeType c = WordAlpha | AntiWordAlpha | JustEscape c
+data EscapeType c = NumAlpha | AntiNumAlpha | JustEscape c
     deriving (Show, Eq)
+data RegToken c = CharRange [c] | SingalChar c | SubExpr [RegToken c] | MultiWaySubExpr [[RegToken c]]
+    deriving (Show, Eq)
+data Suffix = Repeat Integer | RepeatRange (Integer, Integer) | Some | Many | Nil
+
+suffixparser :: Parsec String () Suffix
+suffixparser = undefined
+
+charify :: RegToken c -> IO [c]
+charify t = case t of
+    SingalChar      c -> 
+        return [c]
+
+    CharRange       cs -> do
+        c <- uniform cs
+        return [c]
+
+    SubExpr         ts -> 
+        concat <$> (sequence $ charify <$> ts)
+
+    MultiWaySubExpr tss -> do 
+        ts <- uniform tss
+        concat <$> (sequence $ charify <$> ts)
+
+instance Functor(RegToken) where
+    -- fmap :: (a -> b) -> RegToken a -> RegToken b
+    fmap f r = case r of
+        CharRange cs -> CharRange $ fmap f cs
+        SingalChar c -> SingalChar $ f c
+        SubExpr   es -> SubExpr $ fmap f <$> es
 
 lexer = P.makeTokenParser haskellDef
 integer = P.integer lexer
 braces = P.braces lexer
+parens = P.parens lexer
 brackets = P.brackets lexer
 minusSingal = char '-'
-savedChars = "\\[]~{}"
+savedChars = "\\[]~{}()|"
 intToCh :: Int -> Char
 intToCh = toEnum
-wordAlpha = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
-antiWordAlpha = ['!'..'~'] \\ wordAlpha
+numAlpha = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
+antiNumAlpha = ['!'..'~'] \\ numAlpha
 
-wordRange :: Parsec String () [Char]
+_tokenMerge :: [RegToken c] -> [c]
+_tokenMerge [] = []
+_tokenMerge (SingalChar c: ts) = c  :  _tokenMerge ts
+_tokenMerge (CharRange cs: ts) = cs ++ _tokenMerge ts
+_tokenMerge (_: ts)            = _tokenMerge ts
+
+wordRange :: Parsec String () [RegToken Char]
 wordRange = do
     lowerBound <- anyChar
     minusSingal
     upperBound <- anyChar
-    return [lowerBound..upperBound]
+    return $ [CharRange [lowerBound..upperBound]]
 
-singalWord :: Parsec String () [Char]
-singalWord = (:[]) <$> noneOf "-[]|"
+singalWord :: Parsec String () [RegToken Char]
+singalWord = ((:[]) . SingalChar) <$> noneOf "-[]|"
 
-regRange :: Parsec String () [Char]
-regRange = (nub . mconcat) <$> (brackets $ many (try wordRange <|> singalWord))
+regRange :: Parsec String () [RegToken Char]
+regRange = ((:[]) . CharRange . nub . _tokenMerge . concat) <$> (brackets $ many (try wordRange <|> singalWord))
 
 regRepeat :: Parsec String () Integer
 regRepeat = braces integer' where
     integer' = read <$> (many1 $ oneOf ['0'..'9'])
 
-escape :: Parsec String () (EscapeType Char)
-escape = do
+escape :: Parsec String () [EscapeType Char]
+escape = (:[]) <$> do
     char '\\'
     c <- anyChar
     return $ if 
         |c `elem` savedChars -> JustEscape c
-        |c == 'w' -> WordAlpha
-        |c == 'W' -> AntiWordAlpha
+        |c == 'w' -> NumAlpha
+        |c == 'W' -> AntiNumAlpha
     
-escapeToCharSet :: EscapeType Char -> [Char]
-escapeToCharSet e = case e of
-    JustEscape c -> [c]
-    WordAlpha -> wordAlpha
-    AntiWordAlpha -> antiWordAlpha
+escapeToCharSet :: EscapeType Char -> [RegToken Char]
+escapeToCharSet e = (:[]) $ case e of
+    JustEscape c -> SingalChar c
+    NumAlpha -> CharRange numAlpha
+    AntiNumAlpha -> CharRange antiNumAlpha
 
-attachRepeat :: Parsec String () a -> Parsec String () [a]
+attachRepeat :: Parsec String () [a] -> Parsec String () [a]
 attachRepeat p = do
     item <- p
     repeatTime <- many regRepeat
     let totalRepeatTime = product repeatTime
-    return $ replicate (fromInteger totalRepeatTime) item
+    return $ concat (replicate (fromInteger totalRepeatTime) item)
     
-regRangeWithRepeat :: Parsec String () [[Char]]
+regRangeWithRepeat :: Parsec String () [RegToken Char]
 regRangeWithRepeat = attachRepeat regRange
 
 escapeWithRepeat :: Parsec String () [EscapeType Char]
 escapeWithRepeat = attachRepeat escape
 
-simpleParser :: Parsec String () [[[Char]]]
-simpleParser = many $ simpleChars <|> (fmap escapeToCharSet) <$> escapeWithRepeat <|> regRangeWithRepeat where
-    simpleChars = (:[]) <$> (:[]) <$> noneOf savedChars
+subExpr :: Parsec String () [RegToken Char]
+subExpr =  attachRepeat $ ((:[]) . SubExpr) <$> parens simpleParser
 
+multiWaySubexpr :: Parsec String () [RegToken Char]
+multiWaySubexpr = attachRepeat $ (:[]) . MultiWaySubExpr <$> (parens $ sepBy simpleParser (char '|'))
+
+simpleParser :: Parsec String () [RegToken Char]
+simpleParser = concat <$> (many $ simpleChars <|> (concat . fmap escapeToCharSet) <$> escapeWithRepeat <|> regRangeWithRepeat <|> try subExpr <|>multiWaySubexpr) where
+    simpleChars = ((:[]) . SingalChar) <$> noneOf savedChars
